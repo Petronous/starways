1c65e8658d3d93096cedc687eca20a4b18defd9e
petronous <petron.slonek@gmail.com>
2024-10-04 21:34:22 +0200

    further development of the borders module

--- scenes/borders_tester.gd
+++ scenes/borders_tester.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-
+var a_list: Array[Vector2]
+var b_list: Array[Vector2]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Laser.add_point(position)
 	$Laser.add_point(get_endpoint())
-	
+	for i in range(40):
+		var a := Vector2(randf()*1400 + 200, randf()*800 + 100)
+		a_list.append(a)
+		b_list.append(a + Vector2.from_angle(randf()*2*PI)*(randf()*60 + 40))
+
 
 func get_endpoint() -> Vector2:
 	var mouse := get_local_mouse_position()
@@ -20,6 +25,14 @@ func _process(delta: float) -> void:
 func _draw() -> void:
 	draw_circle($circle.position, 200., Color.LIGHT_CORAL)
 	var res := Borders.get_circle_intersections($circle.position, 200., position, get_local_mouse_position() - position)
+	for i in range(a_list.size()):
+		var bline := Borders.BorderLine.new()
+		bline.vert1 = a_list[i]
+		bline.vert2 = b_list[i]
+		var state := Borders.get_closer($circle.position, bline, position, get_local_mouse_position())
+		draw_line(a_list[i], b_list[i], [Color.RED, Color.GREEN, Color.BLUE][state], 10.)
+		draw_circle(a_list[i], 10, Color.WHITE)
 	if is_nan(res.x): return
 	draw_circle(Vector2(res.x, res.y), 20, Color.BLACK)
 	draw_circle(Vector2(res.z, res.w), 20, Color.WHITE)
+	
--- scripts/borders.gd
+++ scripts/borders.gd
@@ -1,4 +1,4 @@
-class_name Borders extends CanvasItem
+class_name Borders extends Node2D
 
 var points: Array[BorderCell]
 #var verts: PackedVector2Array = []
@@ -30,24 +30,25 @@ static func perpendicular(vec: Vector2) -> Vector2:
 static func get_closer(center: Vector2, border: BorderLine, na: Vector2, nb: Vector2) -> Closer:
 	var ba := border.vert1
 	var bb := border.vert2
-	center -= ba
-	var judge = bb - ba
-	na -= ba
-	nb -= ba
-	# TODO: it's more complicated
-	#var s_c = sign(judge.cross(center))
-	#assert(s_c != 0)
-	#var s_na = sign(judge.cross(na))
-	#var s_nb = sign(judge.cross(nb))
-	#var s_n = sign(s_na + s_nb)
-	#if s_c == s_n: return Closer.NEW
-	#elif s_c == -s_n: return Closer.BORDER
-	#else: return Closer.INTERSECTION
+	center -= na
+	var judge = nb - na
+	ba -= na
+	bb -= na
+	var s_c = sign(judge.cross(center))
+	assert(s_c != 0)
+	var s_ba = sign(judge.cross(ba))
+	var s_bb = sign(judge.cross(bb))
+	var s_b = sign(s_ba + s_bb)
+	if s_c == s_b: 
+		if s_ba == 0 or s_bb == 0: return Closer.INTERSECTION
+		return Closer.BORDER
+	elif s_c == -s_b: return Closer.NEW
+	else: return Closer.INTERSECTION
 	return 0
 
 func _init(from: Array[Node2D], max_radius_: float = 300) -> void:
 	for i in range(from.size()):
-		var npoint = BorderCell.new(i, from[i].position, borders)
+		var npoint = BorderCell.new(i, from[i].position)
 		points.append(npoint)
 	for i in range(points.size()):
 		calculate_borders(i)
@@ -56,24 +57,67 @@ func _init(from: Array[Node2D], max_radius_: float = 300) -> void:
 
 
 func calculate_borders(i: int) -> void:
-	var this: Vector2 = points[i].pos
+	var this := points[i]
 	for j in range(i+1, points.size()):
+		# get a b from intersecting the circle with the this-other bisector
 		var other: Vector2 = points[j].pos
-		if this.distance_squared_to(other) > max_sq_radius: continue
-		var origin: Vector2 = (this + other) / 2.0
-		var dir: Vector2 = perpendicular(other - this)
-		var res := get_circle_intersections(this, max_radius, origin, dir)
+		if this.pos.distance_squared_to(other) > max_sq_radius: continue
+		var origin: Vector2 = (this.pos + other) / 2.0
+		var dir: Vector2 = perpendicular(other - this.pos)
+		var res := get_circle_intersections(this.pos, max_radius, origin, dir)
 		var a := Vector2(res.x, res.y)
 		var b := Vector2(res.z, res.w)
-		var new_borders: Array
+		
+		# determine which borders are shadowed by the new line and which intersect it
+		var new_borders: Array[BorderLine]
+		var intersections: Array[Vector2]
+		var intersected: Array[BorderLine]
+		var is_shadowed_a := false
+		var is_shadowed_b := false
+		for border in this.borders:
+			match get_closer(this.pos, border, a, b):
+				Closer.BORDER: new_borders.append(border)
+				Closer.INTERSECTION: 
+					intersections.append(
+						Geometry2D.segment_intersects_segment(border.vert1, border.vert2, a, b)
+					)
+					intersected.append(border)
+				Closer.NEW: border.destroy(this, borders)
+			if this.raycast(border, a): is_shadowed_a = true
+			if this.raycast(border, b): is_shadowed_b = true
+		
+		# if the new line is too far discard it, if it isn't add it and process any intersections
+		assert(is_shadowed_a == is_shadowed_b or not intersections.is_empty())
+		assert(intersections.size() <= 2)  # both asserts implied by this being voronoi. 
+										   # A voronoi cell is always convex, even when in progress
+		if is_shadowed_a and is_shadowed_b and intersections.is_empty(): continue
+		var n_border: BorderLine
+		n_border.cell1 = points[i] 
+		n_border.cell2 = points[j]
+		if intersections.size() == 0:
+			n_border.vert1 = a
+			n_border.vert2 = b
+		elif intersections.size() == 1:
+			pass
+		else:
+			pass
+
 
 
 class BorderLine extends RefCounted:
 	var id: int
 	var vert1: Vector2
+	var neigh1: BorderLine
 	var vert2: Vector2
-	var cell1: int
-	var cell2: int
+	var neigh2: BorderLine
+	var cell1: BorderCell
+	var cell2: BorderCell
+	
+	func destroy(caller: BorderCell, borders: Array[BorderLine]) -> void:
+		for cell: BorderCell in [cell1, cell2]:
+			if cell == caller: continue
+			cell.borders.erase(self)
+		borders.erase(self)
 
 
 class BorderCell extends RefCounted:
@@ -81,13 +125,14 @@ class BorderCell extends RefCounted:
 	var pos: Vector2
 	#var my_verts: Array[int]
 	#var my_vert_neighs: Array[Vector2i]
-	var my_borders: Array[int]
+	var borders: Array[BorderLine]
 	
 	#var real_verts: PackedVector2Array
-	var real_borders: Array[BorderLine]
 	
-	func _init(id_: int, pos_: Vector2, borders: Array[BorderLine]) -> void:
+	func _init(id_: int, pos_: Vector2) -> void:
 		id = id_
 		pos = pos_
 		#real_verts = verts
-		real_borders = borders
+		
+	func raycast(border: BorderLine, endpoint: Vector2) -> bool:
+		return Geometry2D.segment_intersects_segment(pos, endpoint, border.vert1, border.vert2) != null
